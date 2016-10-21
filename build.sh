9c74f096f0452c7518fdcbf4a2587d6242fc0e55
Steve.Lasker@MSFT <stevelas@microsoft.com>
2016-10-20 23:57:43 -0700

    temp for tags

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 docker-compose -f ./docker-compose.build.yml up
 docker-compose -f ./src/Marketing/docker-compose.yml build
 docker login bikeshare-exp.azurecr.io --username 95392ec2-0ace-480d-9470-2f40c480a4e1 --password bikeshare
-docker tag bikesharing/marketing bikeshare-exp.azurecr.io/bikesharing/marketing:$(BUILD_BUILDNUMBER)
-docker push bikeshare-exp.azurecr.io/bikesharing/marketing:$(BUILD_BUILDNUMBER)
+docker tag bikesharing/marketing bikeshare-exp.azurecr.io/bikesharing/marketing:22
+docker push bikeshare-exp.azurecr.io/bikesharing/marketing:22
 
