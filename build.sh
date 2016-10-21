554a6a893e27cec3a139fba314bd107168c119dc
Steve.Lasker@MSFT <stevelas@microsoft.com>
2016-10-21 01:33:43 -0700

    fix pathing

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 set -e
 dotnet restore3
 rm -rf $(pwd)src/Marketing/bin/Release/PublishOutput
-dotnet publish3 src/Marketing/Marketing.csproj -c release -o $(pwd)src/Marketing/bin/Release/PublishOutput 
+dotnet publish3 src/Marketing/Marketing.csproj -c release -o $(pwd)/src/Marketing/bin/Release/PublishOutput 
