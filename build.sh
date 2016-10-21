a3e8f43f841d180df121782e98035986810de827
Steve.Lasker@MSFT <stevelas@microsoft.com>
2016-10-21 00:10:50 -0700

    echo echo echo

--- build.sh
+++ build.sh
@@ -1,8 +1,17 @@
 #!bin/bash
 set -e
+echo docker-compose -f ./docker-compose.build.yml up 
 docker-compose -f ./docker-compose.build.yml up
+
+echo docker-compose -f ./src/Marketing/docker-compose.yml build
 docker-compose -f ./src/Marketing/docker-compose.yml build
+
+echo docker login bikeshare-exp.azurecr.io --username 95392ec2-0ace-480d-9470-2f40c480a4e1 --password bikeshare
 docker login bikeshare-exp.azurecr.io --username 95392ec2-0ace-480d-9470-2f40c480a4e1 --password bikeshare
+
+echo docker tag bikesharing/marketing bikeshare-exp.azurecr.io/bikesharing/marketing:22
 docker tag bikesharing/marketing bikeshare-exp.azurecr.io/bikesharing/marketing:22
+
+echo docker push bikeshare-exp.azurecr.io/bikesharing/marketing:22
 docker push bikeshare-exp.azurecr.io/bikesharing/marketing:22
 
