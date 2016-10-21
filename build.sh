5ba8f0275efa2c5ad07dd368a97fd1a1a6a294d8
Steve.Lasker@MSFT <stevelas@microsoft.com>
2016-10-21 00:14:49 -0700

    change the working directory

--- build.sh
+++ build.sh
@@ -3,8 +3,10 @@ set -e
 echo docker-compose -f ./docker-compose.build.yml up 
 docker-compose -f ./docker-compose.build.yml up
 
-echo docker-compose -f ./src/Marketing/docker-compose.yml build
-docker-compose -f ./src/Marketing/docker-compose.yml build
+cd ./src/Marketing
+pwd
+echo docker-compose build
+docker-compose  build
 
 echo docker login bikeshare-exp.azurecr.io --username 95392ec2-0ace-480d-9470-2f40c480a4e1 --password bikeshare
 docker login bikeshare-exp.azurecr.io --username 95392ec2-0ace-480d-9470-2f40c480a4e1 --password bikeshare
