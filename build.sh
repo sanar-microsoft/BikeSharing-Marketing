b2aa5562f8274251eba281b85a2069aa762c4f3f
Steve.Lasker@MSFT <stevelas@microsoft.com>
2016-10-21 00:02:41 -0700

    add the failure on error flag

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!bin/bash
+set -e
 docker-compose -f ./docker-compose.build.yml up
 docker-compose -f ./src/Marketing/docker-compose.yml build
 docker login bikeshare-exp.azurecr.io --username 95392ec2-0ace-480d-9470-2f40c480a4e1 --password bikeshare
