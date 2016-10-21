2453d85f9e134890b0d1f3b669f451254243ca2e
Steve.Lasker@MSFT <stevelas@microsoft.com>
2016-10-20 23:55:33 -0700

    Edit the build.sh script

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!bin/bash
 docker-compose -f ./docker-compose.build.yml up
-docker login bikeshare-exp.azurecr.io -u 95392ec2-0ace-480d-9470-2f40c480a4e1 -p bikeshare
-docker tag bikesharing/marketing bikeshare-exp.azurecr.io/bikesharing/marketing:$(Build.BuildNumber)
-docker push bikeshare-exp.azurecr.io/bikesharing/marketing:$(Build.BuildNumber)
+docker-compose -f ./src\Marketing/docker-compose.yml build
+docker login bikeshare-exp.azurecr.io --username 95392ec2-0ace-480d-9470-2f40c480a4e1 --password bikeshare
+docker tag bikesharing/marketing bikeshare-exp.azurecr.io/bikesharing/marketing:$(BUILD_BUILDNUMBER)
+docker push bikeshare-exp.azurecr.io/bikesharing/marketing:$(BUILD_BUILDNUMBER)
+
