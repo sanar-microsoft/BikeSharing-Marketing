faaa9df1009baf1d333e1539b533a971d534d5c3
Steve.Lasker@MSFT <stevelas@microsoft.com>
2016-10-21 00:41:50 -0700

    switch build container styles

--- build.sh
+++ build.sh
@@ -1,24 +1,5 @@
 #!bin/bash
 set -e
-
-echo docker login bikeshare-exp.azurecr.io --username 95392ec2-0ace-480d-9470-2f40c480a4e1 --password bikeshare
-docker login bikeshare-exp.azurecr.io --username 95392ec2-0ace-480d-9470-2f40c480a4e1 --password bikeshare
-
-
-echo docker-compose -f ./docker-compose.build.yml up 
-docker-compose -f ./docker-compose.build.yml up
-
-cd ./src/Marketing
-pwd
-echo docker-compose build
-docker-compose  build
-
-echo docker login bikeshare-exp.azurecr.io --username 95392ec2-0ace-480d-9470-2f40c480a4e1 --password bikeshare
-docker login bikeshare-exp.azurecr.io --username 95392ec2-0ace-480d-9470-2f40c480a4e1 --password bikeshare
-
-echo docker tag bikesharing/marketing bikeshare-exp.azurecr.io/bikesharing/marketing:22
-docker tag bikesharing/marketing bikeshare-exp.azurecr.io/bikesharing/marketing:22
-
-echo docker push bikeshare-exp.azurecr.io/bikesharing/marketing:22
-docker push bikeshare-exp.azurecr.io/bikesharing/marketing:22
-
+dotnet restore3
+rm -rf $(pwd)/publish/marketing
+dotnet publish3 src/Marketing/Marketing.csproj -c release -o $(pwd)/publish/marketing 
