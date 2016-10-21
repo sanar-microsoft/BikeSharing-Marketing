547f6b20777e018e4d0a8cdcb86ce3aeaa92757c
Steve.Lasker@MSFT <stevelas@microsoft.com>
2016-10-21 01:24:12 -0700

    update

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!bin/bash
 set -e
 dotnet restore3
-rm -rf $(pwd)/publish/marketing
-dotnet publish3 src/Marketing/Marketing.csproj -c release -o $(pwd)/publish/marketing 
+rm -rf $(pwd)src/Marketing/bin/Release/PublishOutput
+dotnet publish3 src/Marketing/Marketing.csproj -c release -o $(pwd)src/Marketing/bin/Release/PublishOutput 
